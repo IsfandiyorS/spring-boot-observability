f54c96a8d74f42c68b740787dd1ea42e37e4e1c7
Isfandiyor Sultonov <isfandiyorsultonov18@mail.ru>
2024-02-13 10:06:10 +0500

    Initial commit

---
+++ src/main/java/com/example/grafanawithprometheus/GrafanaWithPrometheusApplication.java
@@ -0,0 +1,13 @@
+package com.example.grafanawithprometheus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrafanaWithPrometheusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrafanaWithPrometheusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/grafanawithprometheus/GrafanaWithPrometheusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grafanawithprometheus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrafanaWithPrometheusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
